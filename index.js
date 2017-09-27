5cbad174bbb65bf0aa09eda3fb2281c2d2864d65
WooodHead <WooodHead@users.noreply.github.com>
2017-09-27 14:58:49 +0800

    dot dot dot

--- index.js
+++ index.js
@@ -84,42 +84,45 @@ function createPagination(req) {
   return function createPagination(pages, current) {
     var params = qs.parse(url.parse(req.url).query)
     var str = ''
+    current = Number(current)
 
     if (current == 1) {
-      // str += '<li class="disabled"><a>First</a></li>'
-      str += '<li class="active"><a>1</a></li>'
+        str += '<li class="active"><a>1</a></li>'
     } else {
-      // str += '<li><a href="/products/1">First</a></li>'
-      params.page = 1
-      var href = '?' + qs.stringify(params)
-      str += '<li><a href="' + href + '">1</a></li>'
+        params.page = 1
+        var href = '?' + qs.stringify(params)
+        str += '<li><a href="' + href + '">1</a></li>'
     }
 
-    var i = (Number(current) > 5 ? Number(current) - 4 + 1 : 2)
-    if (i !== 2) {
-      str += '<li class="disabled"><a>...</a></li>'
+    var beforeAfter = 3
+
+    var i;
+    if (current > beforeAfter + 2) {
+        i = current - beforeAfter
+        str += '<li class="disabled"><a>...</a></li>'
+    } else {
+        i = 2
     }
-    for (; i <= (Number(current) + 4) && i < pages; i++) {
-      if (i == current) {
-        str = str + '<li class="active"><a>' + i + '</a></li>'
-      } else {
-        params.page = i
-        var href = '?' + qs.stringify(params)
-        str += '<li><a href="' + href + '">' + i + '</a></li>'
-      }
-      if (i == Number(current) + 4 && i < pages) {
-        str = str + '<li class="disabled"><a>...</a></li>'
-      }
+
+    for (; i <= (current + beforeAfter) && i < pages; i++) {
+        if (i == current) {
+            str = str + '<li class="active"><a>' + i + '</a></li>'
+        } else {
+            params.page = i
+            var href = '?' + qs.stringify(params)
+            str += '<li><a href="' + href + '">' + i + '</a></li>'
+        }
+        if (i == current + beforeAfter && i < pages - 1) {
+            str = str + '<li class="disabled"><a>...</a></li>'
+        }
     }
 
     if (current == pages) {
-      str += '<li class="active"><a>' + pages + '</a></li>'
-      // str += '<li class="disabled"><a>Last</a></li>'
+        str += '<li class="active"><a>' + pages + '</a></li>'
     } else {
-      params.page = pages
-      var href = '?' + qs.stringify(params)
-      str += '<li><a href="' + href + '">' + pages + '</a></li>'
-      // str += '<li><a href="/products/' + pages + '">Last</a></li>'
+        params.page = pages
+        var href = '?' + qs.stringify(params)
+        str += '<li><a href="' + href + '">' + pages + '</a></li>'
     }
 
     return str
